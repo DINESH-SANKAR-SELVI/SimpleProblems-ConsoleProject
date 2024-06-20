da3626cadcd6aa42579b7b622c1dd963105d653c
DINESH-SANKAR-SELVI <dinesh.sankar0110@gmail.com>
2024-06-20 10:36:04 +0530

    ArmstrongNumber changes

--- src/SIMPLEPROGRAMS/ArmstrongNumber/ArmstrongNumber.java
+++ src/SIMPLEPROGRAMS/ArmstrongNumber/ArmstrongNumber.java
@@ -1,38 +1,30 @@
 package SIMPLEPROGRAMS.ArmstrongNumber;
 
-import java.util.Scanner;
-
-public class ArmstrongNumber{
+public class ArmstrongNumber {
     public int input;
-    public ArmstrongNumber(int ip){
+
+    public ArmstrongNumber(int ip) {
         this.input = ip;
     }
-    public boolean armstrongNumber(){
+
+    public boolean armstrongNumber() {
         int output = this.input;
         int input1 = 0;
         int finalPut = 0;
-        while (true){
-            if(output>9) {
+        while (true) {
+            if (output > 9) {
                 input1 = output % 10;
                 output = output / 10;
                 input1 = (int) Math.pow(input1, 3);
                 finalPut = finalPut + input1;
-            }
-            else{
+            } else {
                 input1 = output;
-                input1 = (int)Math.pow(input1,3);
+                input1 = (int) Math.pow(input1, 3);
                 finalPut = finalPut + input1;
                 break;
             }
         }
-        if(this.input == finalPut) return true ;
+        if (this.input == finalPut) return true;
         else return false;
-    }
-    public static void main(String[] args){
-        Scanner io = new Scanner(System.in);
-        int iput = io.nextInt();
-        ArmstrongNumber k = new ArmstrongNumber(iput);
-         if(k.armstrongNumber()) System.out.println("It's a Armstrong Number : "+k.input);
-         else System.out.println("It's Not a Armstrong Number : "+k.input);
     }
 }
